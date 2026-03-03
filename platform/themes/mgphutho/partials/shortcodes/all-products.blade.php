bd4e15e0b4999bd94e47719fa5f66679d6dcf459
Phong Nguyễn <nhphong0104@gmail.com>
2026-03-03 17:29:16 +0700

    up code

--- platform/themes/mgphutho/partials/shortcodes/all-products.blade.php
+++ platform/themes/mgphutho/partials/shortcodes/all-products.blade.php
@@ -1,16 +1,12 @@
 <div class="section pt-0 small_pb">
     <div class="container">
         @if ($title)
-            <div class="heading_tab_header">
-                <div class="heading_s2">
-                    <h2 class="h4">{!! BaseHelper::clean($title) !!}</h2>
-                </div>
-            </div>
+        <h3 style="text-align:center;padding:20px 0;font-size:30px;">{!! BaseHelper::clean($title) !!}</h3>
         @endif
         @if ($products->isNotEmpty())
             <div class="row">
                 @foreach($products as $product)
-                    <div class="col-md-4 col-6">
+                    <div class="col-6 col-car">
                         {!! Theme::partial('product-item', compact('product')) !!}
                     </div>
                 @endforeach
