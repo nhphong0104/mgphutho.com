c8535136ffc42950336d564abf1628a94bd024b8
Phong Nguyễn <nhphong0104@gmail.com>
2026-03-11 15:05:30 +0700

    up header

--- platform/themes/mgphutho/partials/header.blade.php
+++ platform/themes/mgphutho/partials/header.blade.php
@@ -1,7 +1,9 @@
 <section class="header group">
         <div class="container">
         <div class="logo">
-        <a title="MG Láng Hạ" href="/"><img class="img-logo ls-is-cached lazyloaded" alt="MG Láng Hạ" data-src="https://drive.gianhangvn.com/image/logo-2569256j31415.jpg" src="https://drive.gianhangvn.com/image/logo-2569256j31415.jpg"></a>
+        <a title="MG Láng Hạ" href="/">
+            <img class="img-logo ls-is-cached lazyloaded" src="{{ RvMedia::getImageUrl(theme_option('logo')) }}" alt="{{ theme_option('site_title') }}" loading="lazy" />
+        </a>
     </div>
     <a href="javascript:void(0);" class="cd-nav-toggle"><span class="fa fa-bars"></span></a></div>
 </section>
