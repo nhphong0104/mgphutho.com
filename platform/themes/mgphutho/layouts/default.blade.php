ba103b6c53e7e46e6318f8a5fe95a253c045b0e3
Phong Nguyen <nhphong0104@gmail.com>
2026-03-13 23:33:26 +0700

    up fix body

--- platform/themes/mgphutho/layouts/default.blade.php
+++ platform/themes/mgphutho/layouts/default.blade.php
@@ -6,30 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5, user-scalable=1"
         name="viewport" />
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     {!! Theme::header() !!}
 </head>
 
 <body>
     {!! Theme::partial('header') !!}
-
-    <div class="breadcrumb_section bg_gray page-title-mini">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-md-6">
-                    <div class="page-title">
-                        <h1>{{ Theme::get('pageName') }}</h1>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    {!! Theme::partial('breadcrumbs') !!}
-                </div>
-            </div>
-        </div>
-    </div>
-
     {!! Theme::content() !!}
-
     {!! Theme::partial('footer') !!}
     {!! Theme::footer() !!}
 </body>
