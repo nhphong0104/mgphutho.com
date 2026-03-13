ff324f5cc9c2c11818eb0c819baba74300e799c0
Phong Nguyen <nhphong0104@gmail.com>
2026-03-13 23:36:19 +0700

    up fix body

--- platform/themes/mgphutho/views/page.blade.php
+++ platform/themes/mgphutho/views/page.blade.php
@@ -5,6 +5,12 @@
          {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, Html::tag('div', BaseHelper::clean($page->content), ['class' => 
 'ck-content'])->toHtml(), $page) !!}
     @else
+    <div id="banner-wrapper" class="">
+        <div class="porto-block ">
+            <div class="vc_single_image-wrapper  vc_box_border_grey">
+                <img width="100%" height="457" src="https://mgphutho.com/storage/banners/services.jpg" class="vc_single_image-img attachment-full" alt="banner-tintuc" decoding="async" loading="lazy"></div>
+            </div>
+        </div>
         <div class="section">
             <div class="container">
                 {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, Html::tag('div', BaseHelper::clean($page->content), ['class' => 
