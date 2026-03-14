b1c0e74a5f8ade90e8b691a960d954938b5dbe32
Phong Nguyễn <nhphong0104@gmail.com>
2026-03-14 21:45:06 +0700

    up

--- platform/themes/mgphutho/views/category.blade.php
+++ platform/themes/mgphutho/views/category.blade.php
@@ -2,6 +2,24 @@
     Theme::set('pageName', $category->name);
     Theme::layout('blog-sidebar');
 @endphp
+
+<div id="banner-wrapper" class="">
+    <div class="porto-block ">
+        <div class="vc_single_image-wrapper  vc_box_border_grey">
+            <img width="100%" height="457"  src="https://mgphutho.com/storage/banners/services.jpg"
+                                    class="vc_single_image-img attachment-full" alt="banner-tintuc" decoding="async"
+                                    loading="lazy"></div>
+        </div>
+        <div class="wpb_text_column wpb_content_element  absolute_left" style="left: 175.4px;">
+            <div class="wpb_wrapper">
+                <h1>{{ $category->name }}</h1>
+                    <p>Cung cấp những thông tin mới nhất<br>
+                    về MG Motor.</p>
+            </div>
+	    </div>
+    </div>
+</div>
+
 <div class="container">
     <div class="d-flex align-items-center">
         <div class="block-news__scroll">
