7d9b086277ba8ea26be50a2af44c90594e90848c
Phong Nguyen <nhphong0104@gmail.com>
2026-03-16 00:13:57 +0700

    up

--- platform/themes/mgphutho/views/category.blade.php
+++ platform/themes/mgphutho/views/category.blade.php
@@ -6,24 +6,26 @@
 <div id="banner-wrapper" class="">
     <div class="porto-block ">
         <div class="vc_single_image-wrapper  vc_box_border_grey">
-            <img width="100%" height="457"  src="https://mgphutho.com/storage/banners/services.jpg"
-                                    class="vc_single_image-img attachment-full" alt="banner-tintuc" decoding="async"
-                                    loading="lazy"></div>
+            <img width="100%" height="457" src="https://mgphutho.com/storage/banners/services.jpg"
+                class="vc_single_image-img attachment-full" alt="banner-tintuc" decoding="async" loading="lazy">
         </div>
+    </div>
+    <div class="container">
         <div class="wpb_text_column wpb_content_element  absolute_left" style="left: 175.4px;">
             <div class="wpb_wrapper">
                 <h1>{{ $category->name }}</h1>
-                    <p>Cung cấp những thông tin mới nhất<br>
+                <p>Cung cấp những thông tin mới nhất<br>
                     về MG Motor.</p>
             </div>
-	    </div>
+        </div>
     </div>
+</div>
 </div>
 
 <div class="container">
     <div class="row row-16">
         @include(Theme::getThemeNamespace() . '::views.templates.posts')
         <div class="sp-40"></div>
-        
+
     </div>
 </div>
