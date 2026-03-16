78ccf858f056336409b1332fc7feb24f8461ff18
Phong Nguyễn <nhphong0104@gmail.com>
2026-03-16 09:53:01 +0700

    up

--- platform/themes/mgphutho/layouts/homepage.blade.php
+++ platform/themes/mgphutho/layouts/homepage.blade.php
@@ -1,22 +1,46 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5, user-scalable=1" name="viewport"/>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
 
-        {!! Theme::header() !!}
-    </head>
-    <body>
-        {!! Theme::partial('header') !!}
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5, user-scalable=1"
+        name="viewport" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
-        {!! Theme::content() !!}
+    {!! Theme::header() !!}
+</head>
 
-        {!! Theme::partial('footer') !!}
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-        {!! Theme::footer() !!}
-    </body>
+<body>
+    {!! Theme::partial('header') !!}
+
+    {!! Theme::content() !!}
+
+    {!! Theme::partial('footer') !!}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    {!! Theme::footer() !!}
+    <script>
+        const swiper_block_feature = new Swiper(".swiper-block-feature", {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            pagination: {
+                el: ".swiper-block-feature .swiper-pagination",
+                clickable: true,
+            },
+            breakpoints: {
+                768: {
+                    slidesPerView: 2,
+                    spaceBetween: 0,
+                },
+                992: {
+                    slidesPerView: 4,
+                    spaceBetween: 0,
+                },
+            },
+        });
+    </script>
+</body>
+
 </html>
