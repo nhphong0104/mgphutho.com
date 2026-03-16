03e4330f23d329b8e219472f4d31181cb605d0b0
Phong Nguyễn <nhphong0104@gmail.com>
2026-03-16 09:51:45 +0700

    up

--- platform/themes/mgphutho/layouts/homepage.blade.php
+++ platform/themes/mgphutho/layouts/homepage.blade.php
@@ -16,16 +16,7 @@
 
         {!! Theme::partial('footer') !!}
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-        
-        {!! Theme::footer() !!}
         <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-        <script>
-        const swiper = new Swiper('.swiper', {
-        loop: true,
-        autoplay: {
-            delay: 3000
-        }
-        });
-        </script>
+        {!! Theme::footer() !!}
     </body>
 </html>
