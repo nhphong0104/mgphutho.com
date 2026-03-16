be79fec0aa86ee9e4e6bcfb2b7f3c45dcafe8f34
Phong Nguyễn <nhphong0104@gmail.com>
2026-03-16 09:49:06 +0700

    up

--- platform/themes/mgphutho/layouts/homepage.blade.php
+++ platform/themes/mgphutho/layouts/homepage.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5, user-scalable=1" name="viewport"/>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
         {!! Theme::header() !!}
     </head>
     <body>
@@ -14,6 +16,16 @@
 
         {!! Theme::partial('footer') !!}
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+        
         {!! Theme::footer() !!}
+        <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+        <script>
+        const swiper = new Swiper('.swiper', {
+        loop: true,
+        autoplay: {
+            delay: 3000
+        }
+        });
+        </script>
     </body>
 </html>
