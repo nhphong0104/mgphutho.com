2c3835558bc129730abf87b2cd5817777ce8046a
Phong Nguyễn <nhphong0104@gmail.com>
2026-03-24 14:23:54 +0700

    up

--- platform/themes/mgphutho/partials/product-item.blade.php
+++ platform/themes/mgphutho/partials/product-item.blade.php
@@ -2,7 +2,7 @@
     <div class="category-car">
         <div class="category-car-image">
             <a href="{{ $product->url }}" title="{{ $product->name }}">
-                <img alt="{{ $product->name }}" src="{{ RvMedia::getImageUrl($product->image) }}"/></a>
+                <img width="100%" alt="{{ $product->name }}" src="{{ RvMedia::getImageUrl($product->image) }}"/></a>
         </div>
         <div class="tt-product">
             <h3 class="category-car-name"><a href="{{ $product->url }}" title="{{ $product->name }}">{!! BaseHelper::clean($product->name) !!}</a>
