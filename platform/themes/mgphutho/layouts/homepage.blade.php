de1b57fd995469baeb3ae438b1a670eadf0a581f
Phong Nguyễn <nhphong0104@gmail.com>
2026-04-01 09:28:04 +0700

    up fix:

--- platform/themes/mgphutho/layouts/homepage.blade.php
+++ platform/themes/mgphutho/layouts/homepage.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
 
 <head>
+    @csrf
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5, user-scalable=1"
